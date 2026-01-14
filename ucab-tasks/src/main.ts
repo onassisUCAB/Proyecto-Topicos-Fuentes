304b95883056bb002e6f54d1515e1686d9727bf8
Jesús Acosta <jgacosta.24@est.ucab.edu.ve>
2026-01-14 17:15:27 -0400

    Documentación de main.ts y file-notes.repository.ts
    Documentación JSDOC de "class FileNotesRepository implements NotesRepository" y "bootstrap"

--- ucab-tasks/src/main.ts
+++ ucab-tasks/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+/**
+ * Función para entrar en la aplicación backend de NestJS.
+ * Esto incluye las configuraciones como los Data Transfer Objects,
+ * el Cross-Origin Resource Sharing (CORS) y la documentación Swagger.
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
